b7f5484c8016b4a348b15f459be1794b234ff8ac
BranDaDev <Brandon.bjs11@gmail.com>
2024-08-09 22:01:15 -0400

    Update Camera Controller.cs

--- 1-800-Julez/Assets/Scripts/Camera Controller.cs
+++ 1-800-Julez/Assets/Scripts/Camera Controller.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour
+
+
 {
     public Transform target;
     // Start is called before the first frame update
@@ -14,6 +16,6 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = new Vector3(target.position.x, target.position.y + 2, transform.position.z);
     }
 }
